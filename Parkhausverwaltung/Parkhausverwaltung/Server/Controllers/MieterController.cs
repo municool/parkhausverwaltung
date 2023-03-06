299eb9d289e1c7ab02e8bf88415ac00feb817570
Louis <herren.louis@gmx.ch>
2023-03-06 22:41:08 +0100

    createt mietercontroller with crud functions

---
+++ Parkhausverwaltung/Parkhausverwaltung/Server/Controllers/MieterController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parkhausverwaltung.Server.Infrastructure;
+using Parkhausverwaltung.Shared.Models;
+
+namespace Mieterverwaltung.Server.Controllers
+{
+    public class MieterController : Controller
+    {
+        public MieterController(IDbContextFactory<ParkhausverwaltungContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Mieter> GetAllMieters()
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Mieters.ToList();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Mieter? GetById(int id)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Mieters.FirstOrDefault(v => v.MieterId == id);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Mieter mieter)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                context.Mieters.Add(mieter);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Update(int id, [FromBody] Mieter mieter)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var oldMieter = context.Mieters.Find(mieter.MieterId);
+
+                if (oldMieter != null)
+                {
+                    oldMieter.Name = mieter.Name;
+                    oldMieter.SlotNr = mieter.SlotNr;
+                    oldMieter.EndDate = mieter.EndDate;
+                    oldMieter.PaymentOpen = mieter.PaymentOpen;
+
+                    context.Mieters.Update(oldMieter);
+                    context.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+
+        private readonly IDbContextFactory<ParkhausverwaltungContext> _dbContextFactory;
+    }
+}
